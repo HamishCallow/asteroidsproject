d86d84cce2176fee3d7a3c8f934af060585aa939
HamishCallow <hamish.mcdermott.yt@gmail.com>
2025-08-19 20:26:51 +1000

    added asteroids and asteroidfield

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -23,10 +23,18 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
         return self.rotation
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            return self.move(dt)
+        if keys[pygame.K_s]:
+           return self.move(-dt) 
         if keys[pygame.K_a]:
             return self.rotate(-dt)
         if keys[pygame.K_d]:
-           return self.rotate(dt) 
+           return self.rotate(dt)
